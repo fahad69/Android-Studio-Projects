c2c5e3d0d20d1ed2d4c917ccab4cc367642fc26c
fahad69 <1605069@ugrad.cse.buet.ac.bd>
2021-04-03 01:29:00 +0600

    commit from ide

--- Demo2/app/src/main/java/com/example/demo2/MainActivity.java
+++ Demo2/app/src/main/java/com/example/demo2/MainActivity.java
@@ -7,9 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -17,12 +22,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    FrameLayout frameLayout;
+    BottomSheetBehavior bottomSheetBehavior;
 
+    public static int SPLASH_TIME_OUT = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent homeintent = new Intent(MainActivity.this, Waiting.class);
+        startActivity(homeintent);
+
+
+        frameLayout = findViewById(R.id.bottomsheet1);
+        bottomSheetBehavior = BottomSheetBehavior.from(frameLayout);
+        bottomSheetBehavior.setPeekHeight(200);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
 
         toolbar = findViewById(R.id.toolbar3);
